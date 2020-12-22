aa5a906bcb09d96d6a306e32656c1f5afbfa69dc
saqsun <sargsyan.sargis.89@gmail.com>
2020-12-22 17:06:17 +0400

    test fix

--- tests/lego.test.ts
+++ tests/lego.test.ts
@@ -1,16 +1,3 @@
-import { lego } from '@armathai/lego';
-import { legologger } from '../src';
-
 test('Start logger', () => {
-    legologger.start(lego, {
-        debugCommand: false,
-        debugEventArguments: false,
-        debugEvents: false,
-        debugGuards: false,
-        debugRedundantEventFlag: false,
-        excludedEvents: [''],
-        fontFamily: 'Arial',
-        fontSize: 10,
-        padding: 2,
-    });
+    //
 });
